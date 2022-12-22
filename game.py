ac87b7fe4fb1c4b59bd630a9b196126fe38021fb
Nhat Minh Vu <nhatminh140303@gmail.com>
2022-12-22 17:19:05 +0700

    Initial commit

--- game.py
+++ game.py
@@ -3,42 +3,63 @@ import numpy as np
 
 class Cell:
     ''' A cell represents a location in a two-dimensional field, where
-    coordinates are given by (row, col)
+    coordinates are given by (x, y)
     '''
 
-    def __init__(self, row, col):
-        ''' Initiatlizes a cell with coordinates (row, col)
+    def __init__(self, x, y):
+        ''' Initiatlizes a cell with coordinates (x, y)
         '''
-        self.row = row
-        self.col = col
-        # TODO: your code here
-        pass
+        self.x = x
+        self.y = y
+
+    def get_coord(self):
+        return self.x, self.y
 
 
+field = []
 class Field:
     ''' A field is a rectangular region containing all cells, implemented 
     by a two-dimensional array
     '''
 
-    def __init__(self, num_rows, num_cols):
+    def __init__(self, num_rows, num_cols, width, height):
         ''' Initializes the field
         '''
-        self.num_rows = num_rows
+        self.num_rows = num_rows 
         self.num_cols = num_cols
-        # TODO: your code here
+        self.width = width
+        self.height = height
+        self.field = field
     
-    def create_field(self):
+    def set_field(self):
         ''' Create the field by adding cells to a two-dimensional array
         '''
-        self.field = []
-        for r in range(self.num_rows):
+        for y in range(self.num_rows):
             row = []
-            for c in range(self.num_cols):
+            for x in range(self.num_cols):
                 row.append(Cell(x, y))
             self.field.append(row)
+
+    def get_neighbors(self, cell):
+        '''
+        Get the neighbors list of a given cell
+
+        cell: a Cell object
+        
+        Return: list of neighbor cells
+        '''
+        x, y = cell.get_coord()
+        neighbors = []
+        for row in range(y - 1, y + 2):
+            for col in range(x - 1, x + 1):
+                if ((row != y or col != x) and
+                    (0 <= row < self.num_cols) and
+                    (0 <= col < self.numcols)):
+                    neighbors.append(field[row][col])
+        return neighbors
     
-    def print_field(self, img):
-        ''' Print the field on the terminal for debugging
+    def print_field(self):
+        ''' Print the field on the terminal for debugging purpose
 
         img: pyautogui image
         '''
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import keyboard
 
 
 if __name__ == '__main__':
-    # Get number of rows and columns from input
-    num_rows, num_cols = int(sys.argv[1]), int(sys.argv[2])
-
-    # Get number of mines
+    # Get number of rows, columns and mines
+    num_rows = int(sys.argv[1])
+    num_cols = int(sys.argv[2])
+    num_mines = int(sys.argv[3])
 
     # Get top left corner coordinates of the field by pressing 'enter'
     keyboard.wait('enter')
@@ -17,3 +17,7 @@ if __name__ == '__main__':
     # Get bottom right corner coordinates of the field by pressing 'enter'
     keyboard.wait()
     right, bottom = pyautogui.position()
+
+    # Calculate width and height of a cell
+    width = (right - left) / num_cols
+    height = (bottom - top) / num_rows
